9c2d8b8ade1c23abbaf42a1534d144e10ac80b47
Swayam <158766919+Swayamjimmy@users.noreply.github.com>
2025-07-14 06:32:36 +0530

    undesigned msg board

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+app.use(express.urlencoded({ extended: true })); // To use form inputs in req.body
+
 app.set('views', path.join(__dirname,"views"));
 app.set('view engine', 'ejs');
 
 const messages = [
   {
+    id:1,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id:2,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -23,7 +27,25 @@ app.get('/', (req,res) =>{
      res.render("index", {title: "Mini Messageboard", messages: messages});
     });
 app.get('/new', (req,res) => {
-    res.sendFile(path.join(__dirname,"./routes/new.html"));
+    res.render("form");
 });
+
+app.post('/new',(req,res) => {
+    const {msg, auth} = req.body;
+    messages.push({  id: messages.length + 1, text: msg, user: auth, added: new Date() });
+    res.redirect('/');
+})
+
+app.get('/message/:id', (req, res) => {
+  const messageId = parseInt(req.params.id, 10);
+  const message = messages.find(m => m.id === messageId);
+
+  if (!message) {
+    return res.status(404).send('Message not found');
+  }
+
+  res.render('message', { title: 'Message Details', message });
+});
+
 
 app.listen(3000);
